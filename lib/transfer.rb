9a7a506762f1aa66484662f7456d7cf3bf80fd9e
rosa lee <rosalee0116@gmail.com>
2020-04-14 15:45:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,25 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :reciever, :amount, :status
+
+  def initialize(sender, reciever, amount)
+    @sender = sender
+    @reciever = reciever
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    @sender.valid? && @reciever.valid? && @sender.balance > @amount
+  end
+
+  def execute_transaction
+    if self.valid? && @status == "pending"
+      @sender.balance -= @amount
+      @reciever.balance += @amount
+      @status = "complete"
+    else
+      @status = "reject"
+      return "Tranaction rejected. Please check your account balance."
+    end
 end
