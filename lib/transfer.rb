97bc490e0d67add98b05f0b34001ed679f47e65c
rosa lee <rosalee0116@gmail.com>
2020-04-14 15:50:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else
-      @status = "reject"
+      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
 end
